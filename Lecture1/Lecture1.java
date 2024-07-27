656c779f4508cea06453907c32a0d2386f88bbf9
Nguyen <nguyennikko6829@students.esuhsd.org>
2024-07-26 17:56:00 -0700

    Second Commit

--- Lecture1/Lecture1.java
+++ Lecture1/Lecture1.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        
+        String a; // "Hey, I have a variable that represents a String. It's name is a"
+        
+        a = "TEXT HERE";
+        
         System.out.println("123456");
     }
 }
