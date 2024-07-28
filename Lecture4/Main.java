b3dc34752fe751484de0db3bd0998fa5b9a45603
Nguyen <nguyennikko6829@students.esuhsd.org>
2024-07-27 17:29:16 -0700

    TESTING GITHUB

--- Lecture4/Main.java
+++ Lecture4/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i=1; i<=5; i++){
+        for(int i=4; i<=42; i++){
             foo(i);
         }
 
 
+    
         //Signature is the name of the method and its the lsit of parameter
         // - name
         // - Params
